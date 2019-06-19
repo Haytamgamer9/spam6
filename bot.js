2345c5489389bab2a7949285acc20a54f401373a
Haytamgamer9 <44749438+Haytamgamer9@users.noreply.github.com>
2019-06-19 16:34:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 
 client.on('ready', async() => {
 var server = "590908301252755476"; // ايدي السررفر
-var channel = "513661774638088194";//ايدي الروم
+var channel = "590908301252755479";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحانss الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,سبحان الله,**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
